e16745abe5b105a210a7fdfb9d8dcb811241ce28
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-14 23:36:13 +0300

    Перевод компонента «GuessArtistScreen» в функциональный.

--- src/components/guess-artist-screen/guess-artist-screen.jsx
+++ src/components/guess-artist-screen/guess-artist-screen.jsx
@@ -1,52 +1,50 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class GuessArtistScreen extends PureComponent {
-  render() {
-    const {
-      question,
-      onAnswer,
-      renderAudioPlayer,
-    } = this.props;
+const GuessArtistScreen = (props) => {
+  const {
+    question,
+    onAnswer,
+    renderAudioPlayer,
+  } = props;
 
-    const {
-      song,
-      answers,
-    } = question;
+  const {
+    song,
+    answers,
+  } = question;
 
-    return (
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
+  return (
+    <section className="game__screen">
+      <h2 className="game__title">Кто исполняет эту песню?</h2>
 
-        <div className="game__track">
-          <div className="track">
-            {renderAudioPlayer(song.src, 0)}
-          </div>
+      <div className="game__track">
+        <div className="track">
+          {renderAudioPlayer(song.src, 0)}
         </div>
+      </div>
 
-        <form className="game__artist">
-          {answers.map((answer, i) => (
-            <div key={answer.artist} className="artist">
-              <input
-                className="artist__input visually-hidden"
-                type="radio"
-                name="answer"
-                value={`artist-${i}`}
-                id={`answer-${i}`}
-                onChange={() => onAnswer(question, answer)}
-              />
+      <form className="game__artist">
+        {answers.map((answer, i) => (
+          <div key={answer.artist} className="artist">
+            <input
+              className="artist__input visually-hidden"
+              type="radio"
+              name="answer"
+              value={`artist-${i}`}
+              id={`answer-${i}`}
+              onChange={() => onAnswer(question, answer)}
+            />
 
-              <label className="artist__name" htmlFor={`answer-${i}`}>
-                <img className="artist__picture" src={answer.picture} alt={answer.artist}/>
-                {answer.artist}
-              </label>
-            </div>
-          ))}
-        </form>
-      </section>
-    );
-  }
-}
+            <label className="artist__name" htmlFor={`answer-${i}`}>
+              <img className="artist__picture" src={answer.picture} alt={answer.artist}/>
+              {answer.artist}
+            </label>
+          </div>
+        ))}
+      </form>
+    </section>
+  );
+};
 
 GuessArtistScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
