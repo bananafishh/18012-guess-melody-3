e752a5c8a4dc0b9bb3d125b8a286d7df8c615065
z.dyshekova <z.dyshekova@fun-box.ru>
2020-03-17 19:00:33 +0300

    Добавление ActionCreator.

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -8,6 +8,17 @@ const ActionType = {
   INCREMENT_STEP: `INCREMENT_STEP`,
 };
 
+const ActionCreator = {
+  incrementErrorsCount: () => ({
+    type: ActionType.INCREMENT_ERRORS_COUNT,
+    payload: 1,
+  }),
+  incrementStep: () => ({
+    type: ActionType.INCREMENT_STEP,
+    payload: 1,
+  }),
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.INCREMENT_ERRORS_COUNT:
@@ -21,4 +32,4 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-export {reducer, ActionType};
+export {reducer, ActionType, ActionCreator};
--- src/reducer/reducer.test.js
+++ src/reducer/reducer.test.js
@@ -1,34 +1,52 @@
-import {reducer, ActionType} from './reducer';
+import {reducer, ActionType, ActionCreator} from './reducer';
 
-it(`Редьюсер, вызванный без аргумента action, возвращает исходное состояние приложения`, () => {
-  expect(reducer(void 0, {})).toEqual({
-    errorsCount: 0,
-    step: -1,
+describe(`Редьюсер работает корректно`, () => {
+  it(`Редьюсер, вызванный без аргумента action, возвращает исходное состояние приложения`, () => {
+    expect(reducer(void 0, {})).toEqual({
+      errorsCount: 0,
+      step: -1,
+    });
+  });
+
+  it(`Редьюсер увеличивает количество совершённых игроком ошибок на переданное значение`, () => {
+    expect(reducer({
+      errorsCount: 0,
+      step: -1,
+    }, {
+      type: ActionType.INCREMENT_ERRORS_COUNT,
+      payload: 1,
+    })).toEqual({
+      errorsCount: 1,
+      step: -1,
+    });
+  });
+
+  it(`Редьюсер увеличивает шаг игры на переданное значение`, () => {
+    expect(reducer({
+      errorsCount: 0,
+      step: -1,
+    }, {
+      type: ActionType.INCREMENT_STEP,
+      payload: 1,
+    })).toEqual({
+      errorsCount: 0,
+      step: 0,
+    });
   });
 });
 
-it(`Редьюсер увеличивает количество совершённых игроком ошибок на переданное значение`, () => {
-  expect(reducer({
-    errorsCount: 0,
-    step: -1,
-  }, {
-    type: ActionType.INCREMENT_ERRORS_COUNT,
-    payload: 1,
-  })).toEqual({
-    errorsCount: 1,
-    step: -1,
+describe(`Action creators работают корректно`, () => {
+  it(`Action creator для увеличения количества совершённых ошибок возвращает правильный action`, () => {
+    expect(ActionCreator.incrementErrorsCount()).toEqual({
+      type: ActionType.INCREMENT_ERRORS_COUNT,
+      payload: 1,
+    });
   });
-});
 
-it(`Редьюсер увеличивает шаг игры на переданное значение`, () => {
-  expect(reducer({
-    errorsCount: 0,
-    step: -1,
-  }, {
-    type: ActionType.INCREMENT_STEP,
-    payload: 1,
-  })).toEqual({
-    errorsCount: 0,
-    step: 0,
+  it(`Action creator для увеличения шага игры возвращает правильный action`, () => {
+    expect(ActionCreator.incrementStep()).toEqual({
+      type: ActionType.INCREMENT_STEP,
+      payload: 1,
+    });
   });
 });
